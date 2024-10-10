5a28f19df9f68074dd6e9b45480928b8b8b4c2bf
Subarukunowo <sandymuliakesuma@gmail.com>
2024-10-10 19:04:20 +0700

    first commit

---
+++ app/src/main/java/com/sandy/intentexample2/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.sandy.intentexample2
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sandy.intentexample2.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val receiveData = intent.getStringExtra("main_activity_data")
+        binding.textView.text = receiveData
+    }
+}
